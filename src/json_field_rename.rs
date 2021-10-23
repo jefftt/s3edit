a3c45731a91b85f66d7952181d2a9ab7a89d7055
Jeff Lai <jeff@oxio.io>
2021-10-23 16:33:14 -0400

    add json_field_raname command

---
+++ src/json_field_rename.rs
@@ -0,0 +1,84 @@
+use crate::{JsonFieldRenameParams, S3Url};
+use anyhow::{anyhow, Result};
+use aws_sdk_s3 as s3;
+use futures::{stream, StreamExt};
+use serde_json::Value;
+
+pub(crate) async fn run(
+    client: s3::Client,
+    s3url: &S3Url,
+    params: JsonFieldRenameParams,
+) -> Result<()> {
+    println!("{} {}", s3url.bucket, s3url.prefix);
+    let mut page_token = None;
+    let mut files = Vec::new();
+    loop {
+        let req = client
+            .list_objects_v2()
+            .bucket(&s3url.bucket)
+            .prefix(&s3url.prefix)
+            .set_continuation_token(page_token);
+        let resp = req.send().await?;
+        let keys: Vec<String> = resp
+            .contents
+            .unwrap_or_else(|| Vec::new())
+            .iter()
+            .filter_map(|md| md.key.as_ref().map(|s| s.clone()))
+            .collect();
+        files.extend_from_slice(&keys);
+        if let Some(token) = resp.next_continuation_token {
+            page_token = Some(token)
+        } else {
+            break;
+        }
+    }
+
+    stream::iter(&files)
+        .map(|file| process(&client, &s3url.bucket, &file, &params))
+        .buffered(params.concurrency)
+        .collect::<Vec<Result<()>>>()
+        .await
+        .into_iter()
+        .collect::<Result<Vec<()>>>()?;
+    Ok(())
+}
+
+async fn process(
+    client: &s3::Client,
+    bucket: &str,
+    file: &str,
+    params: &JsonFieldRenameParams,
+) -> Result<()> {
+    let response = client.get_object().bucket(bucket).key(file).send().await?;
+    let raw_body = &response.body.collect().await?.into_bytes();
+    let jsonlines = serde_json::Deserializer::from_slice(&raw_body).into_iter::<Value>();
+
+    let overwrite = jsonlines.fold(Vec::new(), |mut acc, json| {
+        let renamed = rename(json.unwrap(), &params.source, &params.target).unwrap();
+        acc.extend_from_slice(&serde_json::to_vec(&renamed).unwrap());
+        acc.push(b'\n');
+        acc
+    });
+
+    client
+        .put_object()
+        .bucket(bucket)
+        .key(file)
+        .body(s3::ByteStream::from(overwrite))
+        .send()
+        .await?;
+
+    Ok(())
+}
+
+// TODO: support nested json
+fn rename(mut json: Value, source: &str, target: &str) -> Result<Value> {
+    if let Some(root) = json.as_object_mut() {
+        if let Some(value) = root.remove(source) {
+            root.insert(target.to_string(), value);
+            return Ok(json);
+        }
+    }
+
+    return Err(anyhow!("field not found"));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,88 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{anyhow, Result};
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_s3 as s3;
+use clap::{App, AppSettings, Arg, SubCommand};
+use url::Url;
+
+mod json_field_rename;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let opt = parse_args()?;
+
+    let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
+    let shared_config = aws_config::from_env().region(region_provider).load().await;
+    let client = s3::Client::new(&shared_config);
+
+    match opt.command {
+        Command::JsonFieldRename(params) => {
+            json_field_rename::run(client, &opt.s3url, params).await?
+        }
+    }
+    Ok(())
+}
+
+fn parse_args() -> Result<Opt> {
+    let matches = App::new("s3edit")
+        .version("0.0.1")
+        .about("Bulk edit s3 files")
+        .arg(Arg::from_usage(
+            "--url=<url> 's3 url to apply edits to recursively'",
+        ))
+        .setting(AppSettings::SubcommandRequired)
+        .subcommand(
+            SubCommand::with_name("json-field-rename")
+                .about("rename json field for jsonline files")
+                .arg(Arg::from_usage("--source <source> 'original field name, currently only supports flat JSON structure'"))
+                .arg(Arg::from_usage("--target <target> 'new field name'"))
+                .arg(Arg::from_usage(
+                    "-p, --parallelism n 'max number of files to process at any given time, defaults to 5'",
+                )),
+        )
+        .get_matches();
+
+    let s3url = {
+        let url = Url::parse(matches.value_of("url").unwrap()).unwrap();
+        if url.scheme() != "s3" {
+            panic!("unsupported url");
+        }
+
+        S3Url {
+            bucket: url.host_str().unwrap().to_string(),
+            prefix: url.path().trim_start_matches("/").to_string(),
+        }
+    };
+
+    let command = {
+        match matches.subcommand() {
+            ("json-field-rename", Some(sub_m)) => Command::JsonFieldRename(JsonFieldRenameParams {
+                source: sub_m.value_of("source").unwrap().to_string(),
+                target: sub_m.value_of("target").unwrap().to_string(),
+                concurrency: sub_m.value_of("parallel").map_or(5, |v| v.parse().unwrap()),
+            }),
+            (command, _) => return Err(anyhow!("Unknown command: {}", command)),
+        }
+    };
+
+    Ok(Opt { s3url, command })
+}
+
+struct S3Url {
+    bucket: String,
+    prefix: String,
+}
+
+struct Opt {
+    s3url: S3Url,
+    command: Command,
+}
+
+enum Command {
+    JsonFieldRename(JsonFieldRenameParams),
+}
+
+pub(crate) struct JsonFieldRenameParams {
+    pub(crate) source: String,
+    pub(crate) target: String,
+    pub(crate) concurrency: usize,
 }
