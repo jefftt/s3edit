96452dcca2e84c1357e1ac3a7d2bcac114153048
Jeff Lai <jeff@oxio.io>
2021-10-23 17:06:09 -0400

    Add homebrew support

---
+++ pkg/brew/s3edit-bin.rb
@@ -0,0 +1,17 @@
+class RipgrepBin < Formula
+  version '0.0.1'
+  desc "Make bulk S3 edits"
+  homepage "https://github.com/jefftt/s3edit"
+
+  if OS.mac?
+      url "https://github.com/jefftt/s3edit/releases/download/#{version}/s3edit-#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "7f73d029053a77e7d60564f03dd9e40fef5b210a38282249d4ebca552de8e847"
+  end
+
+  conflicts_with "s3edit"
+
+  def install
+    bin.install "s3edit"
+  end
+end
+
