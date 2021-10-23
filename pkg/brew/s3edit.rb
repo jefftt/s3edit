a908ffe505d31048649684461f2d51e83dfb0d32
Jeff Lai <jeff@oxio.io>
2021-10-23 17:11:16 -0400

    fix s3edit-bin.rb

--- pkg/brew/s3edit.rb
+++ pkg/brew/s3edit.rb
@@ -1,4 +1,4 @@
-class RipgrepBin < Formula
+class S3edit < Formula
   version '0.0.1'
   desc "Make bulk S3 edits"
   homepage "https://github.com/jefftt/s3edit"
@@ -7,8 +7,6 @@ class RipgrepBin < Formula
       url "https://github.com/jefftt/s3edit/releases/download/#{version}/s3edit-#{version}-x86_64-apple-darwin.tar.gz"
       sha256 "7f73d029053a77e7d60564f03dd9e40fef5b210a38282249d4ebca552de8e847"
   end
-
-  conflicts_with "s3edit"
 
   def install
     bin.install "s3edit"
